1e7f67fa171cd9ef0e8b3cec7bfe05146bed2848
Timo <6156589+Shik3i@users.noreply.github.com>
2026-04-22 11:31:44 +0200

    Update domain blacklist with search engines, storage, and e-commerce

--- shared/blacklist.js
+++ shared/blacklist.js
@@ -5,12 +5,30 @@
  * These are typically sites that won't contain shareable video content.
  */
 export const BLACKLIST_DOMAINS = [
+    // Search Engines & Portals
+    'google.com',
+    'bing.com',
+    'duckduckgo.com',
+    'yahoo.com',
+    'msn.com',
+    'baidu.com',
+    'yandex.ru',
+
     // Mail Providers
     'mail.google.com',
     'outlook.live.com',
     'outlook.office.com',
     'gmx.net',
     'web.de',
+
+    // Cloud Storage & Documents
+    'docs.google.com',
+    'sheets.google.com',
+    'slides.google.com',
+    'drive.google.com',
+    'dropbox.com',
+    'onedrive.live.com',
+    'icloud.com',
 
     // Messengers
     'web.whatsapp.com',
@@ -29,18 +47,28 @@ export const BLACKLIST_DOMAINS = [
     'github.com',
     'gitlab.com',
     'bitbucket.org',
+    'stackoverflow.com',
 
-    // Social Media
+    // Social Media & Forums
     'linkedin.com',
     'twitter.com',
     'x.com',
     'facebook.com',
     'instagram.com',
+    'reddit.com',
+    'quora.com',
+
+    // E-Commerce
+    'amazon.',
+    'ebay.com',
+    'aliexpress.com',
+    'etsy.com',
 
     // Development & Utilities
     'timer.shik3i.net',
     'localhost',
     'zoom.us',
     'teams.microsoft.com',
-    'meet.google.com'
+    'meet.google.com',
+    'chrome.google.com'
 ];
