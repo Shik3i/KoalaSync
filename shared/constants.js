77e3e829c64431197addbfc1926f42df8c4307eb
GitHub Action <action@github.com>
2026-09-01 05:14:36 +0000

    chore(release): update versions to v3.2.0 [skip ci]

--- shared/constants.js
+++ shared/constants.js
@@ -7,7 +7,7 @@
  */
 
 export const PROTOCOL_VERSION = "1.0.0";
-export const APP_VERSION = "3.1.5";
+export const APP_VERSION = "3.2.0";
 
 export const OFFICIAL_SERVER_URL = 'wss://syncserver.koalastuff.net';
 export const OFFICIAL_LANDING_PAGE_URL = 'https://sync.koalastuff.net';
